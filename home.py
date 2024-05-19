29d47802e7a70112218d95b73482f36c1031aa31
novimadelena <novitamadelena@gmail.com>
2024-05-19 10:11:13 +0700

    strimlite upload photo

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Novi Madelena')
+st.subheader('NIM : 312210470')
+st.subheader('Kelas : TI.22.C6')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/novi2 (2).jpeg", caption="Novi Madelena", use_column_width="Always", width=200)
+
+st.link_button("Github", "https://github.com/NoviMadelena")
+st.link_button("Instagram", "https://www.instagram.com/novimadalena_?igsh=MXN2eWRhM3pyOHRveQ==")
